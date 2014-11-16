2843aa9e5d3d9a39629bd485d7f25c243d673bb1
alextaylor <alexmctaylorpants@gmail.com>
2014-11-16 08:45:57 -0800

    Adds Indicator class

---
+++ CharacterTests/AMBIndicator.h
@@ -0,0 +1,13 @@
+//
+//  AMBIndicator.h
+//  CharacterTests
+//
+//  Created by Alex Taylor on 2014-11-16.
+//  Copyright (c) 2014 Alex Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMBIndicator : NSObject
+
+@end
---
+++ CharacterTests/AMBIndicator.m
@@ -0,0 +1,13 @@
+//
+//  AMBIndicator.m
+//  CharacterTests
+//
+//  Created by Alex Taylor on 2014-11-16.
+//  Copyright (c) 2014 Alex Taylor. All rights reserved.
+//
+
+#import "AMBIndicator.h"
+
+@implementation AMBIndicator
+
+@end
