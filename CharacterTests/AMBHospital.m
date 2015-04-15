7535576d22792249f26a5d0a66690a81f7c1e7ae
alextaylor <alexmctaylorpants@gmail.com>
2015-04-15 14:12:28 -0700

    Hospital is now just a physics body

--- CharacterTests/AMBHospital.m
+++ CharacterTests/AMBHospital.m
@@ -11,14 +11,12 @@
 @implementation AMBHospital
 
 - (instancetype) init {
-    SKTexture *hospitalTexture = [SKTexture textureWithImageNamed:@"hospital"]; // TODO: shared asset loading
-    
-    if (self = [super initWithTexture:hospitalTexture]) {
+    if (self = [super initWithColor:[SKColor whiteColor] size:CGSizeMake(256, 256)]) {
         
         self.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.size.width, self.size.height)]; // the hospital entity is the invisible area that the patient can be dropped off within. the actual graphic is simply placed on the tilemap.
         self.physicsBody.categoryBitMask = categoryHospital;
         self.physicsBody.collisionBitMask = 0x00000000;
-//        self.hidden = YES;
+        self.hidden = YES;
     
     }
     
